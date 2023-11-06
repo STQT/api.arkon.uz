53d2c9df422a13194bf2cc17d801e5ab8aeef057
Kairat Sultan <garri.sultonov@mail.ru>
2023-11-07 02:34:38 +0600

    local changes

--- config/settings/base.py
+++ config/settings/base.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 APPS_DIR = BASE_DIR / "apps"
 env = environ.Env()
 
-READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=False)
+READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=True)
 if READ_DOT_ENV_FILE:
     # OS environment variables take precedence over variables from .env
     env.read_env(str(BASE_DIR / ".env"))
@@ -304,7 +304,7 @@ SPECTACULAR_SETTINGS = {
     "TITLE": "Arkon Dashboard API",
     "DESCRIPTION": "Documentation of API endpoints of Arkon Dashboard",
     "VERSION": "1.0.0",
-    "SERVE_PERMISSIONS": ["rest_framework.permissions.IsAdminUser"],
+    "SERVE_PERMISSIONS": ["rest_framework.permissions.AllowAny"],
 }
 # Your stuff...
 # ------------------------------------------------------------------------------
