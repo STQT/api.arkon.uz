8a7947d52190685bc3a1cb38e9763707d0487762
STQT <productgsm@gmail.com>
2023-11-07 05:17:58 +0500

    fix static_url

--- config/settings/production.py
+++ config/settings/production.py
@@ -156,4 +156,4 @@ SPECTACULAR_SETTINGS["SERVERS"] = [  # noqa: F405
 # ------------------------------------------------------------------------------
 CORS_ALLOW_ALL_ORIGINS = True  # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
-STATIC_URL = "staticfiles"
+STATIC_URL = "/staticfiles/"
