d4705bfe08cf2f0eee69c8e84e9dfb6a1052362b
STQT <productgsm@gmail.com>
2023-12-25 00:27:35 +0500

    dynamically all moments

--- apps/stones/models.py
+++ apps/stones/models.py
@@ -10,19 +10,29 @@ from apps.utils.models import BaseModel
 
 class Brand(BaseModel):
     name = models.CharField("Название", max_length=100)
+    name_color = models.CharField("Цвет названия", max_length=10, default="#FFFFFF")
     slogan = models.CharField("Слоган", max_length=100)
-    icon = models.ImageField("Иконка в поиске", upload_to="brands/icons")
+    slogan_color = models.CharField("Цвет слогана", max_length=10, default="#FFFFFF")
     phone = models.IntegerField("Телефон", default=991979899,
                                 help_text="Образец: 991979899")
     address = models.CharField("Адрес", max_length=50, default="6A Лабзак, Ташкент")
-    email_support = models.CharField("Официальный сайт", default="example.com", max_length=100)
-    email = models.EmailField("E-mail для связи", default="example@email.com")
-    location_url = models.URLField("Ссылка для локации (Google Maps, Yandex)", null=True, blank=True)
+    product_label = models.CharField(verbose_name="Заголовок блока продуктов", max_length=100,
+                                     null=True, blank=True, default="Products")
     category = models.ForeignKey(Category, verbose_name="Категория", related_name="brands",
                                  null=True, blank=True,
                                  on_delete=models.SET_NULL)
-    country = CountryField(default="UZ")
+    category_label = models.CharField(verbose_name="Заголовок блока коллекций", max_length=100,
+                                      null=True, blank=True, default="Collections")
     description = RichTextField(verbose_name="Описание", null=True, blank=True)
+    information_label = models.CharField("Заголовок блока информаций", max_length=100,
+                                         default="INFORMATION OF COMPANY", null=True, blank=True)
+    email_support = models.CharField("Официальный сайт",
+                                     default="arkon.uz", max_length=100,
+                                     null=True, blank=True)
+    email = models.EmailField("E-mail для связи", default="info@arkon.uz", blank=True, null=True)
+    location_url = models.URLField("Ссылка для локации (Google Maps, Yandex)", null=True, blank=True)
+    country = CountryField(default="UZ")
+    icon = models.ImageField("Иконка в поиске", upload_to="brands/icons")
     logo = models.ImageField(verbose_name="Логотип", upload_to="stones/brand")
     logo_thumbnail = ImageSpecField(source='logo',
                                     format='PNG',
@@ -71,7 +81,10 @@ class BrandLocations(models.Model):
 
 class Categories(BaseModel):
     brand = models.ForeignKey(Brand, verbose_name="Бренд", related_name="categories", on_delete=models.CASCADE)
-    name = models.CharField(verbose_name="Название", max_length=100)
+    name = models.CharField(verbose_name="Название", max_length=100, blank=True, null=True)
+    name_color = models.CharField(verbose_name="Цвет названия", max_length=10, default="#FFFFFF")
+    product_label = models.CharField(verbose_name="Заголовок блока продуктов", max_length=10,
+                                     null=True, blank=True, default="Products")
     image = models.ImageField(verbose_name="Изображение", upload_to="stones/categories")
     image_thumbnail = ImageSpecField(source='image',
                                      format='JPEG',
@@ -82,7 +95,10 @@ class Categories(BaseModel):
         verbose_name_plural = "Модели "
 
     def __str__(self):
-        return self.name
+        try:
+            return self.name
+        except:
+            return None
 
 
 class Product(BaseModel):
@@ -91,7 +107,9 @@ class Product(BaseModel):
     brand = models.ForeignKey(Brand, verbose_name="Бренд", on_delete=models.SET_NULL, null=True, blank=True,
                               help_text="Если продукт не имеет модели, вы можете указать бренд",
                               related_name="products")
-    name = models.CharField(verbose_name="Название", max_length=100)
+    name = models.CharField(verbose_name="Название", max_length=100, blank=True, null=True)
+    name_color = models.CharField(verbose_name="Цвет названия", max_length=10, default="#FFFFFF")
+
     image = models.ImageField(verbose_name="Изображение", upload_to="stones/products")
     image_thumbnail = ImageSpecField(source='image',
                                      format='JPEG',
@@ -102,7 +120,10 @@ class Product(BaseModel):
         verbose_name_plural = "Продукты "
 
     def __str__(self):
-        return self.name
+        try:
+            return self.name
+        except:
+            return None
 
 
 class ProductShots(models.Model):
